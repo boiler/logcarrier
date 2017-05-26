81e8e0fb0dce14e6a7d2dc3820e9064c29ab706c
Denis Cheremisov <d.cheremisov@corp.mail.ru>
2017-05-26 18:36:15 +0300

    Compressed frames must not be splitted

---
+++ src/frameio/frameio.go
@@ -0,0 +1,71 @@
+/*
+package frameio
+Bufferized writer that keeps integrity of blocks produced by data compressors.
+This is important, since decomporessors usually work upon the stream of data splitted
+into frames, where the frame is a solid chunk that is required to be a single piece to be
+decompressed.
+*/
+
+package frameio
+
+import (
+	"bytes"
+	"io"
+)
+
+const (
+	defaultBufferSize = 128 * 1024 * 1024
+)
+
+// Writer is a bufferized writer which takes care of comporession frames integrity, i.e.
+// it only splits data at frame bounds, not at frame internals.
+type Writer struct {
+	bufsize int
+	writer  io.Writer
+	buffer  *bytes.Buffer
+}
+
+// NewWriter constructs writer whose buffer has the default size
+func NewWriter(writer io.Writer) *Writer {
+	return NewWriterSize(writer, defaultBufferSize)
+}
+
+// NewWriterSize return a new writer whose buffer has at least specified size
+func NewWriterSize(writer io.Writer, size int) *Writer {
+	res := &Writer{
+		bufsize: size,
+		writer:  writer,
+		buffer:  &bytes.Buffer{},
+	}
+	res.buffer.Grow(size)
+	return res
+}
+
+// Flush flushes all buffered data
+func (w *Writer) Flush() error {
+	if w.buffer.Len() > 0 {
+		_, err := io.Copy(w.writer, w.buffer)
+		if err != nil {
+			return err
+		}
+	}
+	w.buffer.Reset()
+	return nil
+}
+
+// Write writes the content of data into the buffer
+func (w *Writer) Write(data []byte) (nn int, err error) {
+	if w.buffer.Len() > 0 && w.buffer.Len()+len(data) > w.bufsize {
+		err = w.Flush()
+		if err != nil {
+			return
+		}
+		w.buffer.Reset()
+	}
+	if len(data) > w.bufsize {
+		nn, err = w.writer.Write(data)
+		return
+	}
+	nn, err = w.buffer.Write(data)
+	return
+}
