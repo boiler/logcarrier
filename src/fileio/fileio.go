904ff7741653213a05f91ead6d87741f5a016755
Denis Cheremisov <d.cheremisov@corp.mail.ru>
2017-05-28 20:57:40 +0300

    Root path helper

--- src/fileio/fileio.go
+++ src/fileio/fileio.go
@@ -5,26 +5,30 @@ package fileio
 package fileio
 
 import (
+	"fmt"
 	"os"
+	"utils"
 )
 
 // File object that is steady against rotating.
 type File struct {
 	flags int
 	mode  os.FileMode
+	root  Root
+
 	fpath string
 
 	file *os.File
 }
 
 // Open opens a file with default flags and mode
-func Open(fpath string) (*File, error) {
-	return OpenFile(fpath, os.O_CREATE|os.O_APPEND|os.O_RDWR, os.FileMode(0644))
+func Open(root Root, fpath string) (*File, error) {
+	return OpenFile(root, fpath, os.O_CREATE|os.O_APPEND|os.O_RDWR, os.FileMode(0644))
 }
 
 // OpenFile with given flags and mode
-func OpenFile(fpath string, flags int, mode os.FileMode) (*File, error) {
-	file, err := os.OpenFile(fpath, flags, mode)
+func OpenFile(root Root, fpath string, flags int, mode os.FileMode) (*File, error) {
+	file, err := root.OpenFile(fpath, flags, mode)
 	if err != nil {
 		return nil, err
 	}
@@ -32,6 +36,7 @@ func OpenFile(fpath string, flags int, mode os.FileMode) (*File, error) {
 		flags: flags,
 		mode:  mode,
 		fpath: fpath,
+		root:  root,
 
 		file: file,
 	}, nil
@@ -40,7 +45,7 @@ func OpenFile(fpath string, flags int, mode os.FileMode) (*File, error) {
 // Write ...
 func (f *File) Write(p []byte) (n int, err error) {
 	if f.file == nil {
-		if f.file, err = os.OpenFile(f.fpath, f.flags, f.mode); err != nil {
+		if f.file, err = f.root.OpenFile(f.fpath, f.flags, f.mode); err != nil {
 			return
 		}
 	}
@@ -54,4 +59,19 @@ func (f *File) Close() error {
 	}
 	f.file = nil
 	return nil
+}
+
+// Logrotate ...
+func (f *File) Logrotate(newpath string) error {
+	if f.file != nil {
+		panic(fmt.Errorf("File `%s` had to be closed before the log rotation into `%s`", newpath, f.fpath))
+	}
+	if !utils.PathExists(f.root.Path(f.fpath)) {
+		return fmt.Errorf("Can't rename file %s: file not exists", f.fpath)
+	}
+	if utils.PathExists(f.root.Path(newpath)) {
+		return fmt.Errorf("Can't rename file %s => %s: file exists", f.fpath, newpath)
+	}
+	err := os.Rename(f.root.Path(f.fpath), f.root.Path(newpath))
+	return err
 }
