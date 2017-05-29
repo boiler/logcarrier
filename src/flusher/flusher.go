f02bc7afd64509fb3e0926b764d180e52d28fca9
Denis Cheremisov <d.cheremisov@corp.mail.ru>
2017-05-29 12:20:48 +0300

    Clean the mess

--- src/flusher/flusher.go
+++
@@ -1,9 +0,0 @@
-package flusher
-
-// Flusher interface has two methods:
-//  regular buffer flushing
-//  checks if forced flushing makes a sense
-type Flusher interface {
-	Flush() error
-	WorthFlushing() bool
-}
